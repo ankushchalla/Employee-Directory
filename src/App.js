bf428d2ac0c23e1c7b3618c9ce8bf272dfff735f
Ankush <ankushchalla@gmail.com>
2021-03-26 13:22:42 -0700

    config gh pages.

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
                         firstName: result.name.first,
                         lastName: result.name.last,
                         email: result.email,
+                        // 'State' as in California, not a React state. 
                         state: result.location.state,
                         id: result.login.uuid
                     }
@@ -21,9 +22,37 @@ function App() {
             })
     }, [])
 
+    const sortByLastName = (event) => {
+        // Setting state must always involve creating a new object, array, etc. 
+        let sortedDirectory = directoryState.map(employee => employee)
+            .sort((a, b) => a.lastName >= b.lastName ? 1 : -1);
+        setDirectoryState(sortedDirectory);
+    }
+
+    // Backspace doesn't work since you're permanently changing state (until reload).
+    const filterByState = (event) => {
+        // 'State' as in California, not a React state. 
+        let state = event.target.value.toLowerCase();
+        setDirectoryState(directoryState.filter(employee => {
+            return employee.state.toLowerCase().includes(state);
+        }))
+    }
+
     return (
         <div className="container mt-4">
-            <h1>Employee Directory</h1>
+            <div className="row">
+                <div className="col">
+                    <h1>Employee Directory</h1>
+                </div>
+                <div className="col-3"><button type="button" class="btn btn-primary" onClick={(e) => sortByLastName(e)}>Sort by last name.</button></div>
+                <div className="col-3">
+                    <div class="input-group mb-3">
+                        <input type="text" onChange={(e) => { filterByState(e) }} className="form-control" placeholder="Filter by state." />
+                    </div>
+                </div>
+
+
+            </div>
             <table className="table">
                 <thead>
                     <tr>
